a6ac43e75c680f09651426ea96358ebf931b6c70
Kemboi Elvis <kemboielvis22@gmail.com>
2026-01-01 16:45:33 +0300

    fix: rename to correct name

---
+++ ghostty-web-nuxt/app/app.vue
@@ -0,0 +1,60 @@
+<script setup lang="ts">
+import { init, Terminal } from "ghostty-web";
+
+const terminalRef = ref<HTMLDivElement | null>(null);
+let term: Terminal;
+let ws: WebSocket;
+
+onMounted(async () => {
+  await init();
+  term = new Terminal({
+    fontSize: 14,
+    cursorBlink: true,
+    cols: 80,
+    rows: 40,
+  });
+
+  if (terminalRef.value) {
+    term.open(terminalRef.value);
+    term.write("Welcome to Ghostty web \r \n");
+    term.write("Connecting.... \r\n");
+  }
+  let protocol = window.location.protocol;
+  ws = new WebSocket(
+    `${protocol == "https" ? "wss" : "ws"}://${window.location.host}/_ws`,
+  );
+
+  ws.onopen = (e) => {
+    term.write("Connected\r\n");
+  };
+
+  ws.onmessage = (e) => {
+    const msg = JSON.parse(e.data);
+    if (msg.type == "data") {
+      term.write(msg.data);
+    }
+  };
+
+  ws.onerror = () => {
+    term.write("\r\nWebSocket error \r\n");
+  };
+
+  term.onData((data) => {
+    ws.send(
+      JSON.stringify({
+        type: "command",
+        data,
+      }),
+    );
+  });
+});
+</script>
+<template>
+  <div>
+    <p>Simple web terminal implimntation using ghossty web</p>
+  </div>
+  <div>
+    <NuxtRouteAnnouncer />
+    <div ref="terminalRef" />
+  </div>
+</template>
